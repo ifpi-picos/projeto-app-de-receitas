fee10385e5b191271b78c84c3a9df4bf20a67582
hipolitovivian <125373072+hipolitovivian@users.noreply.github.com>
2023-06-28 16:32:07 -0300

    Update receita.js

--- receita.js
+++ receita.js
@@ -3,3 +3,19 @@ const fs = require('fs')
 
 let nome = prompt('informe o nome: ')
 console.log(nome);
+
+const receitas = [];
+
+function cadastrarReceita(receita){
+this.receita.push(receita)
+}
+
+function removerReceita(receita){
+    this.receita=this.receitas.filter(receita => receita.nome !==nome);
+}
+
+exibirReceitas (){
+    console.log("Receitas")
+    this.receitas.forEach.receitas;
+}
+

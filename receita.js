563098b9f39d07e626e0e2c079d05a1958c37c11
hipolitovivian <125373072+hipolitovivian@users.noreply.github.com>
2023-06-28 17:42:18 -0300

    Update receita.js

--- receita.js
+++ receita.js
@@ -1,21 +1,66 @@
 const prompt = require('prompt-sync')();
-const fs = require('fs')
+const fs = require('fs');
 
-let nome = prompt('informe o nome: ')
+let nome = prompt('informe o nome da preparação: ');
 console.log(nome);
 
-const receitas = [];
+const receita = [];
 
 function cadastrarReceita(receita){
 this.receita.push(receita)
-}
+};
 
 function removerReceita(receita){
     this.receita=this.receitas.filter(receita => receita.nome !==nome);
-}
+};
 
-exibirReceitas (){
-    console.log("Receitas")
-    this.receitas.forEach.receitas;
-}
+function exibirReceitas(receita){
+    console.log("receita"),
 
+    nome = nome,
+    dataCadastro = data,
+    descricao = descrição,
+    ingredientes = ingredientes,
+    modoDePreparo = instruções
+
+    return {nome, dataCadastro, descricao, ingredientes, modoDePreparo}
+
+};
+
+function listarReceitasPorNome(){
+    if (receita.length === 0) {
+      console.log("Nenhuma receita cadastrada!");
+      return;
+    };
+  
+    const receitasOrdenadas = receita.slice().sort((a, b) =>
+      a.nome.localeCompare(b.nome)
+    );
+  
+    console.log("Receitas ordenadas por nome:");
+    receitasOrdenadas.forEach((receita) => {
+      console.log("Nome:", receita.nome);
+      console.log("Descrição:", receita.descricao);
+      console.log("Data de Cadastro:", receita.dataCadastro);
+      console.log("**********Bom apetite!**********");
+    });
+  };
+  
+function listarReceitasPorData(){
+    if (data.length === 0) {
+        console.log("Nenhuma receita cadastrada nessa data!");
+        return;
+    }
+    
+    const receitasData = receita.slice().sort((a, b) =>
+    a.data.localeCompare(b.data)
+  );
+
+  console.log("Receitas ordenadas por data:");
+    receitasOrdenadas.forEach((receita) => {
+      console.log("Nome:", receita.nome);
+      console.log("Descrição:", receita.descricao);
+      console.log("Data de Cadastro:", receita.dataCadastro);
+      console.log("**********Bom apetite!**********");
+    });
+};
